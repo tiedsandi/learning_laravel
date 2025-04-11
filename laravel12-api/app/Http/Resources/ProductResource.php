16873de8b5d6df258f6e538b92f1523608d4bfb7
tiedsandi <fachransandi@gmail.com>
2025-04-11 07:08:55 +0700

    Add custom response structure to ProductResource with status and message

--- laravel12-api/app/Http/Resources/ProductResource.php
+++ laravel12-api/app/Http/Resources/ProductResource.php
@@ -7,13 +7,38 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class ProductResource extends JsonResource
 {
+    //define properti
+    public $status;
+    public $message;
+    public $resource;
+
     /**
-     * Transform the resource into an array.
+     * __construct
      *
-     * @return array<string, mixed>
+     * @param  mixed $status
+     * @param  mixed $message
+     * @param  mixed $resource
+     * @return void
+     */
+    public function __construct($status, $message, $resource)
+    {
+        parent::__construct($resource);
+        $this->status  = $status;
+        $this->message = $message;
+    }
+
+    /**
+     * toArray
+     *
+     * @param  mixed $request
+     * @return array
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'success'   => $this->status,
+            'message'   => $this->message,
+            'data'      => $this->resource
+        ];
     }
 }
