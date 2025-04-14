14c3f28da67de75deb52f457c48e4009085b6efc
tiedsandi <fachransandi@gmail.com>
2025-04-14 11:49:39 +0700

    Implement user registration functionality in AuthController with validation

--- absensi-api/app/Http/Controllers/API/AuthController.php
+++ absensi-api/app/Http/Controllers/API/AuthController.php
@@ -3,9 +3,30 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request)
+    {
+        $validation  = Validator::make($request->all(), [
+            'name' => 'required|string',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|string|min:8|confirmed'
+        ]);
+
+        if ($validation->fails()) {
+            return response()->json($validation->errors(), 200);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => $request->password,
+        ]);
+
+        return response()->json(['message' => 'Validation passed', 'user' => $user, 'token' => ''], 201);
+    }
 }
